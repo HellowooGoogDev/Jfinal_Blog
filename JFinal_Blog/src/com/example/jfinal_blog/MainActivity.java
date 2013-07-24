3954fb07c6f26ecd667697e1fd2f35286bf2e5e7
and-oneone <rain@rain-PC>
2013-07-24 21:57:56 +0800

    测试：listView初始化

--- JFinal_Blog/src/com/example/jfinal_blog/MainActivity.java
+++ JFinal_Blog/src/com/example/jfinal_blog/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.jfinal_blog;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+    private ListView listView;
+    private Handler handler;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +20,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	private void initWidget(){
+		listView=(ListView)findViewById(R.id.listView);
+		
+		
 	}
 
 }
